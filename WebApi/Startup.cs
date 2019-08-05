c5e2e452c3e66d33f15d061b1a73b18155bb6850
fickojuventino <filipjovanovicc23@gmail.com>
2019-08-04 17:10:41 -0700

    Switched from .net 1.0 to 2.0

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -4,53 +4,96 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.AspNetCore.Http;
 
 namespace WebApi
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-
-            if (env.IsEnvironment("Development"))
-            {
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-                builder.AddApplicationInsightsSettings(developerMode: true);
-            }
-
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddApplicationInsightsTelemetry(Configuration);
-
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-            app.UseApplicationInsightsRequestTelemetry();
-
-            app.UseApplicationInsightsExceptionTelemetry();
-
+            app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.Run(async(context) => {
+                //Demo: Basic ODP.NET Core application for ASP.NET Core
+                // to connect, query, and return results to a web page
+
+                //Create a connection to Oracle			
+                string conString = "User Id=S15315;Password=S15315;" +
+
+                //How to connect to an Oracle DB without SQL*Net configuration file
+                //  also known as tnsnames.ora.
+                "Data Source=gislab-oracle.elfak.ni.ac.rs:1521/SBP_PDB;";
+
+                //How to connect to an Oracle DB with a DB alias.
+                //Uncomment below and comment above.
+                //"Data Source=<service name alias>;";
+
+                using (OracleConnection con = new OracleConnection(conString))
+                {
+                    using (OracleCommand cmd = con.CreateCommand())
+                    {
+                        try
+                        {
+                            con.Open();
+                            cmd.BindByName = true;                            
+
+                            //Use the command to display employee names from 
+                            // the EMPLOYEES table
+                            cmd.CommandText = "select vrednost from elementc where id = 1";
+
+                            // Assign id to the department number 50 
+                            OracleParameter id = new OracleParameter("id", 50);
+                            cmd.Parameters.Add(id);
+
+                            //Execute the command and use DataReader to display the data
+                            OracleDataReader reader = cmd.ExecuteReader();
+                            while (reader.Read())
+                            {
+                                await context.Response.WriteAsync("Employee First Name: " + reader.GetValue(0) + "\n");
+                            }
+
+                            reader.Dispose();
+                        }
+                        catch (Exception ex)
+                        {
+                            await context.Response.WriteAsync(ex.Message);
+                        }
+                    }
+                }
+            });
         }
     }
 }
